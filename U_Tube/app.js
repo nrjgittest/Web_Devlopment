aab8bdba103bcfcd6c1163a7f9d5e5ad61218740
nrjgittest <133510927+nrjgittest@users.noreply.github.com>
2023-09-22 23:09:08 +0530

    Add files via upload

---
+++ U_Tube/app.js
@@ -0,0 +1,10 @@
+// JavaScript Code 
+var menuicon = document.querySelector(".menuicon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+menuicon.onclick = function(){
+    sidebar.classList.toggle("smallsidebar");
+    container.classList.toggle("largecontainer")
+}
+
